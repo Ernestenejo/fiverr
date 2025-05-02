e51c1110b32d0154e300589031fc2f244e724ccf
Ernestenejo <eenejo69@gmail.com>
2025-05-03 00:35:07 +0100

    new update

--- database.js/sequelize.js
+++
@@ -1,6 +0,0 @@
-const { Sequelize } = require('sequelize')
-
-exports.sequelize = new Sequelize('database_development', 'root', 'root',{
-host:'localhost',
-dialect:'mysql'
-})
--- server.js
+++ server.js
@@ -1,22 +1,28 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const sequelize = require('./database.js/sequelize')
+const sequelize = require('./database/sequelize')
 
-const PORT = 5059
+const PORT = process.env.PORT
 
 app.use(express.json())
 
+
+
+
 const server = async ()=>{
     try {
+        app.listen(PORT,()=>{
+            console.log(`app is listening to PORT: ${PORT}`)
+        })
+        
         await sequelize.authenticate();
         console.log('connected to database successfully')
+        await sequelize.sync
+        
     } catch (error) {
         console.log('unable to connect to database' + error)
     }
     
 }
 server()
-
-app.listen(PORT,()=>{
-    console.log(`app is listening to PORT: ${PORT}`)
-})
